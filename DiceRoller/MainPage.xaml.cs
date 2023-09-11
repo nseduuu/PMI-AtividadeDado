8f71de60e3466abc763227c6ec35661327c09fe7
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2023-09-11 20:47:36 -0300

    [FEAT] correção feita em sala de aula

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -9,16 +9,21 @@ public partial class MainPage : ContentPage
 
     public void OnClicked(object sender, EventArgs e)
 	{
-        int min = 1;
-        var numLista = picker.SelectedItem;
+        //int min = 1;
+        //var numLista = SeletorDeLadosDoDado.SelectedItem;
 
-        int max = int.Parse(numLista.ToString());
+        //int max = int.Parse(numLista.ToString());
 
-        Random rnd= new Random();
+        //Random rnd= new Random();
 
-        int numeroGerado = rnd.Next(min, max);
-        numeroTela.Text = numeroGerado.ToString();
-        
+        //int numeroGerado = rnd.Next(min, max);
+        //numeroSorteado.Text = numeroGerado.ToString();
+
+        var maxValue = SeletorDeLadosDoDado.SelectedItem;
+        var numeroSorteado = new Random().Next(1, (int)maxValue);
+
+        NumeroSorteado.Text = numeroSorteado.ToString();
+
     }
 }
 
