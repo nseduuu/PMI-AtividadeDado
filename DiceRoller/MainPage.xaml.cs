0969cbf5ed3f7320e7930ee3b4d8a932b5e192e7
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2023-09-11 22:40:56 -0300

    [FEAT] Organizando código

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -24,8 +24,6 @@ public partial class MainPage : ContentPage
         }
     }
 
-        
-
     public void OnClicked(object sender, EventArgs e)
 	{
         int numSides = (int)SeletorDeLadosDoDado.SelectedItem;
@@ -36,30 +34,6 @@ public partial class MainPage : ContentPage
 
         diceImage.Source = ImageSource.FromFile($"dice_d{numSides}.png");
 
-        //Dice dado = new Dice((int)SeletorDeLadosDoDado.SelectedItem); // aqui o valor de lados do dado vai ser definido pelo picker
-
-        //var numeroSorteado = dado.RollDice(); // aqui o numero sorteado vai receber o metodo random 
-
-        //NumeroSorteado.Text = numeroSorteado.ToString(); // aqui o valor do NumeroSorteado recebe o numeroSorteado pelo metodo random
-
-
-
-        //int min = 1;
-        //var numLista = SeletorDeLadosDoDado.SelectedItem;
-
-        //int max = int.Parse(numLista.ToString());
-
-        //Random rnd= new Random();
-
-        //int numeroGerado = rnd.Next(min, max);
-        //numeroSorteado.Text = numeroGerado.ToString();
-
-
-        //Correção
-        //var maxValue = SeletorDeLadosDoDado.SelectedItem;
-        //var numeroSorteado = new Random().Next(1, (int)maxValue + 1);
-
-        //NumeroSorteado.Text = numeroSorteado.ToString();
 
     }
 }
